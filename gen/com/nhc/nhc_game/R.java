59e6cff41a0a98a07b71a3362bf3a14bc16a19ec
mohak92 <mohak_tamhane@me.com>
2014-03-12 21:39:20 -0400

    Login Interface

--- gen/com/nhc/nhc_game/R.java
+++ gen/com/nhc/nhc_game/R.java
@@ -14,15 +14,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int Label=0x7f060009;
+        public static final int RememberMe=0x7f060007;
+        public static final int loginView_login_button=0x7f060006;
         public static final int loginView_name_input=0x7f060002;
         public static final int loginView_name_label=0x7f060001;
         public static final int loginView_password_input=0x7f060004;
         public static final int loginView_password_label=0x7f060003;
         public static final int loginView_signup_button=0x7f060005;
+        public static final int myTextView=0x7f060008;
+        public static final int nBack=0x7f06000f;
+        public static final int nClear=0x7f06000e;
+        public static final int nConfiPass=0x7f06000c;
+        public static final int nPassword=0x7f06000b;
+        public static final int nRegister=0x7f06000d;
+        public static final int nUsername=0x7f06000a;
         public static final int view1=0x7f060000;
     }
     public static final class layout {
         public static final int login=0x7f030000;
+        public static final int profile=0x7f030001;
+        public static final int register=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
---
+++ src/com/nhc/nhc_game/view/ProfileView.java
@@ -0,0 +1,24 @@
+package com.nhc.nhc_game.view;
+
+import com.nhc.nhc_game.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ProfileView extends Activity {
+	
+	public TextView helloTextView;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+        initControls();
+    }
+	    
+	public void initControls(){
+		helloTextView = (TextView)findViewById(R.id.myTextView);
+	}
+}
+	
+
